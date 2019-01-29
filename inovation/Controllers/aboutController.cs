6774f9e60e8877d0e6be55f17f5b7f7569f5b29c
Kaustubh Bagwe <kaustubh_bagwe@yahoo.com>
2019-01-30 00:45:14 +0530

    30th jan Design

---
+++ inovation/Controllers/aboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inovation.Controllers
+{
+    public class aboutController : Controller
+    {
+        // GET: about
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult dr_Swagata_Tambe()
+        {
+            return View();
+        }
+
+
+    }
+}
